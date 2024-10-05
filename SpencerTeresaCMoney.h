8b21c6c937dde56fba904a95f807553ebb9f52f2
Teresa <teresalandkamer@gmail.com>
2024-10-04 19:39:29 -0500

    first commit

---
+++ SpencerTeresaCMoney.h
@@ -0,0 +1,23 @@
+class Money {
+    public:
+
+    // Overloaded friend operators
+    friend Money operator +(const Money& amount1, const Money& amount2);
+    friend Money operator -(const Money& amount1, const Money& amount2);
+    friend Money operator -(const Money& amount);
+    friend bool operator ==(const Money& amount1, const Money& amount2);
+
+    // Constructors
+    Money(long dollars, int cents);
+    Money(long dollars);
+    Money();
+
+    double getValue() const;
+
+    friend istream& operator >>(istream& ins, Money& amount);
+    friend ostream& operator <<(ostream& outs, const Money& amount);
+
+    private:
+    long allCents;
+
+};
---
+++ SpencerTeresaCProj04.cpp
@@ -0,0 +1,38 @@
+// Author: Teresa Spencer
+// Date: October 4th, 2024
+// Description: 
+
+#include "SpencerTeresaCMoney.h"
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cctype>
+using namespace std;
+
+int main() {
+    
+}
+
+
+
+/*
+Complete Practice Program #3 on page 738. You may watch video notes on this. However, no sample code is given for this project, 
+as the main intent of the project is for the student to create a "project" on their own.
+
+You are to rework Practice Program #1 from Chapter 1, page 695. You modify the Money class from Display 11.8 adding the following:
+
+operators: <, <=, >, >=
+add member function Percent (int percentFigure) const;returns a percentage of the money amount in the calling object. 
+If purse is a Money object whose value is $900.00 and percentFigure is 10, you return $90.00, 10 percent of $900.00
+Submit three files:
+
+YourLastNameFirstNameInitialMoney.cpp - This file contains only the Money class implementation
+YourLastNameFirstNameInitialProj4.cpp - This file contains only the main function to use/test your money class
+YourLastNameFirstNameInitialMoney.h - This file contains the header file information.  
+Please upload the 3 files separately on the Project 4 link. 
+If you have issues uploading them separately, you can zip them into one and upload. 
+The name of the zip file should be YourLastNameFirstNameInitialProj4.zip. 
+The files contained in the zip file must be named as above. 
+
+
+*/
